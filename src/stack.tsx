ac6890f0538bb1f4d60a6a585b087eff03fe6e5d
brian890809 <brian08090809@gmail.com>
2025-04-24 22:57:25 -0700

    reconfigured stack server app

--- src/stack.tsx
+++ src/stack.tsx
@@ -1,7 +1,16 @@
 import "server-only";
 
 import { StackServerApp } from "@stackframe/stack";
+import { SPOTIFY_SCOPES } from '@/lib/spotify-scope';
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
+  urls: {
+    afterSignIn: "/dashboard",
+    afterSignUp: "/dashboard",
+    afterSignOut: "/"
+  },
+  oauthScopesOnSignIn: {
+    spotify: SPOTIFY_SCOPES
+  }
 });
