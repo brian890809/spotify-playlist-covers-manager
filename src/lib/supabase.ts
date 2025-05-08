ed6928932269eff0c59c6851d4f2afc6a47c6298
brian890809 <brian08090809@gmail.com>
2025-05-08 14:10:29 -0700

    move supabaseAdmin function to server side

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,18 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
 import { createBrowserClient } from "@supabase/ssr";
 import { getSupabaseJwt } from "@/utils/actions";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-// For admin usage
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
-
-// Create a Supabase client with the service role key for server-side operations
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
-});
 
 // Create a Supabase client for client-side operations
 export const createSupabaseClient = () => {
